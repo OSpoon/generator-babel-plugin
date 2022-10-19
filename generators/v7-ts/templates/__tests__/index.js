2832a595f00b08a9e9652a4dc2b4c6d4d8a76c0e
小鑫同学 <zxin088@gmail.com>
2022-10-19 20:50:10 +0800

    feat: 增加自定义单元测试中的Diff显示，并修改原单元测试文件的换行模式

--- generators/v7-ts/templates/__tests__/index.js
+++ generators/v7-ts/templates/__tests__/index.js
@@ -1,20 +1,44 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import { diffChars } from 'diff';
-import { transformFileSync } from '@babel/core';
+import * as path from "path";
+import * as fs from "fs";
+import { diffChars } from "diff";
+import { transformFileSync } from "@babel/core";
+import _console from "console";
 
-describe('', () => {
-  const fixturesDir = path.join(__dirname, 'fixtures');
+const _color = (input, style = 1, color = 92) => {
+  return `\x1b[7;${color};${style};1m${input}\x1b[0m`;
+};
+
+const _print = (diff) => {
+  _console.log(_color("PS：", 0, 100));
+  _console.log(_color(" red for additions, yellow for deletions", 0, 100));
+  _console.log(_color("------------start-------------", 0, 100));
+  let parts = "";
+  diff &&
+    diff.forEach((part) => {
+      parts += _color(
+        part.value,
+        part.removed ? 9 : 1,
+        part.added ? 91 : part.removed ? 93 : 92
+      );
+    });
+  _console.log(parts);
+  _console.log(_color("-------------over-------------", 0, 100));
+};
+
+describe("", () => {
+  const fixturesDir = path.join(__dirname, "fixtures");
   fs.readdirSync(fixturesDir).map((caseName) => {
     const fixtureDir = path.join(fixturesDir, caseName);
     if (!fs.statSync(fixtureDir).isDirectory()) return;
-    
-    it(`should ${caseName.split('-').join(' ')}`, () => {
-      const actualPath = path.join(fixtureDir, 'actual.ts');
+
+    it(`should ${caseName.split("-").join(" ")}`, () => {
+      const actualPath = path.join(fixtureDir, "actual.ts");
       const actual = transformFileSync(actualPath).code;
-      const expected = fs.readFileSync(path.join(fixtureDir, 'expected.ts')).toString();
+      const expected = fs
+        .readFileSync(path.join(fixtureDir, "expected.ts"))
+        .toString();
       const diff = diffChars(actual, expected);
-      diff.length > 1 && console.log(diff);
+      diff.length > 1 && _print(diff);
       expect(diff.length).toBe(1);
     });
   });
